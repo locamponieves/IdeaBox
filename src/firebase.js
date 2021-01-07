7b2613c3812d97827485ac9ab86eecb0d5362e16
Leonis Ocampo Nieves <leonis.ocampo@gmail.com>
2021-01-07 16:29:58 -0500

    Agregando la esrtuctura del proyecto

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAbvWgwNJwtNN3WHyRoTTrv1IvpSB6qAng",
+    authDomain: "ideabox-e5951.firebaseapp.com",
+    projectId: "ideabox-e5951",
+    storageBucket: "ideabox-e5951.appspot.com",
+    messagingSenderId: "857672804465",
+    appId: "1:857672804465:web:b8b17823a079a34f0413d0"
+};
+
+firebase.initializeApp(firebaseConfig);
